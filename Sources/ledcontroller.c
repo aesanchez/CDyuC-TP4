ad35fa5465ab99de9ca2462bf79f3eded97b31cd
Agustin Sanchez <trorik23@gmail.com>
2017-07-01 13:13:43 -0300

    ahora se tiene un arreglo interno rgb

--- Sources/ledcontroller.c
+++ Sources/ledcontroller.c
@@ -15,17 +15,12 @@
 #define BLINK_MIN 100
 #define BLINK_STEP 300
 
+#define RED 0
+#define GREEN 1
+#define BLUE 2
+
 typedef enum{OFF,NORMAL,BLINKING,SWEEPING} state;
 state current_state;
-
-//variables mundanas
-static char key;
-char led_intensity;//intensidad total del sistema
-int blink_period = 700;
-unsigned int blink_counter;
-char unsigned blink_state;//1->ON
-char aux;
-unsigned char sweep_counter;
 
 typedef struct led_type{
     char duty_cycle;
@@ -34,8 +29,15 @@ typedef struct led_type{
     char previous_state;//para el blink
     char mask;//mascara para acceder al valor
 }
+led_type rgb[3];
 
-led_type red, green, blue;
+//variables mundanas
+static char key;
+char led_intensity;//intensidad total del sistema
+int blink_period = 700;
+unsigned int blink_counter;
+char unsigned blink_state;//1->ON
+unsigned char sweep_counter;
 
 /*
 KEY MAP
@@ -62,10 +64,7 @@ void ledcontroller_run(){
         if(key!='*') return; //ignorar todo menos ON
         state_on();
     }else{
-        //leer todo menos la tecla on
         if(key=='*') return; //ignorar los ON
-        //a lo tosco seria algo asi
-        aux=(key-'0') mod 3; //me devuelve al led que corresponde 1/2/3
         switch(key){
             case '#': state_off();      break;
             case 'A': blink_vel_up();   break;
@@ -75,13 +74,13 @@ void ledcontroller_run(){
             case '0': set_white();      break;
             case:'1':
             case:'2':
-            case:'3': led_toggle_state(led pertinente); break;
+            case:'3': led_toggle_state(rgb[(key-'0') mod 3]); break;
             case:'4':
             case:'5':
-            case:'6': led_intensity_up(led_pertinente); break;
+            case:'6': led_intensity_up(rgb[(key-'0') mod 3]); break;
             case:'7':
             case:'8':
-            case:'9': led_intensity_down(del led que corresponda);break;
+            case:'9': led_intensity_down(rgb[(key-'0') mod 3]);break;
             default: /*error*/ break;
         }
     }
@@ -94,62 +93,62 @@ void state_on(void){
 void state_off(void){
     current_state=OFF;
     //apagar forzadamente los leds por si estaban a la mitad de un pwm o algo
-    led_off(red);
-    led_off(blue);
-    led_off(green);
+    led_off(RED);
+    led_off(BLUE);
+    led_off(GREEN);
 }
 
-void fWHITE(void){
+void set_white(void){
     current_state=NORMAL;
-    red.duty_cycle=MAX_CYCLE;
-    green.duty_cycle=MAX_CYCLE;
-    blue.duty_cycle=MAX_CYCLE;
-    led_activate(red);
-    led_activate(green);
-    led_activate(blue);
+    rgb[RED].duty_cycle=MAX_CYCLE;
+    rgb[GREEN].duty_cycle=MAX_CYCLE;
+    rgb[BLUE].duty_cycle=MAX_CYCLE;
+    led_activate(RED);
+    led_activate(GREEN);
+    led_activate(BLUE);
 }
 
-void ledcontroller_init(){
+void ledcontroller_init(void){
     current_state=OFF;
     //configurar los puertos como salida
     PTCDD_PTCDD1=1;
     PTCDD_PTCDD2=1;
     PTCDD_PTCDD3=1;
     //las mascaras del bit que corresponden en PTC
-    red.mask=PTCD_PTCD1_MASK;
-    green.mask=PTCD_PTCD2_MASK;
-    blue.mask=PTCD_PTCD3_MASK;
+    rgb[RED].mask=PTCD_PTCD1_MASK;
+    rgb[GREEN].mask=PTCD_PTCD2_MASK;
+    rgb[BLUE].mask=PTCD_PTCD3_MASK;
     //que arranquen prendidos todos
-    red.duty_cycle=MAX_DUTY_CYCLE;
-    green.duty_cycle=MAX_DUTY_CYCLE;
-    blue.duty_cycle=MAX_DUTY_CYCLE;
+    rgb[RED].duty_cycle=MAX_DUTY_CYCLE;
+    rgb[GREEN].duty_cycle=MAX_DUTY_CYCLE;
+    rgb[BLUE].duty_cycle=MAX_DUTY_CYCLE;
     //los activamos//se podria hacer led_activate();
-    red.state=1;
-    green.state=1;
-    blue.state=1;
+    rgb[RED].state=1;
+    rgb[GREEN].state=1;
+    rgb[BLUE].state=1;
     //led_intensity=INTENSITY_SCALE;
     //arranca fully bright? el tema es que de toque
     // se va a llamar a la funcion para actualizar esto y no nos cambia en nada
 }
 
-void led_desactivate(led_type led){
-    led.state=0;
-    led_off(led);//forzar la salida para que se apague    
+void led_desactivate(char led_index){
+    rgb[led_index].state=0;
+    led_off(led_index);//forzar la salida para que se apague    
 }
 
-void led_activate(led_type led){
-    led.state=1;
+void led_activate(char led_index){
+    rgb[led_index].state=1;
     //no hace falta forzar a "prendido" porque el mismo pwm lo va a prender
 }
 
-void led_toggle_state(led_type led){
-    if(led.state) led_desactivate(led);
-    else led_activate(led);
+void led_toggle_state(char led_index){
+    if(rgb[led_index].state) led_desactivate(led_index);
+    else led_activate(led_index);
 }
 
-void led_on(led_type led){
+void led_on(char led_index){
     //led on=0
-    PTCD= PTCD & (~led.mask)
+    PTCD= PTCD & (~rgb[led_index].mask)
     /*
     PTCD  xxxxxxxx
     and
@@ -159,9 +158,9 @@ void led_on(led_type led){
     */    
 }
 
-void led_off(led_type led){
+void led_off(char led_index){
     //led off=1
-    PTCD = PTCD | led.mask;
+    PTCD = PTCD | rgb[led_index].mask;
     /*
     PTCD  xxxxxxxx
     or
@@ -171,35 +170,35 @@ void led_off(led_type led){
     */
 }
 
-void led_intensity_up(led_type led){
-    if(!led.state) return; //do nothing
-    if(led.duty_cycle==MAX_DUTY_CYCLE) return;
-    led.duty_cycle++;
+void led_intensity_up(char led_index){
+    if(!rgb[led_index].state) return; //do nothing
+    if(rgb[led_index].duty_cycle==MAX_DUTY_CYCLE) return;
+    rgb[led_index].duty_cycle++;
 }
 
-void led_intensity_down(led_type led){
-    if(!led.state) return; //do nothing
-    if(led.duty_cycle==MIN_DUTY_CYCLE) return;
-    led.duty_cycle--;
+void led_intensity_down(char led_index){
+    if(!rgb[led_index].state) return; //do nothing
+    if(rgb[led_index].duty_cycle==MIN_DUTY_CYCLE) return;
+    rgb[led_index].duty_cycle--;
 }
 
 void ledcontroller_interrupt_handler(void){//llamada cada 1 ms
     if(current_state==OFF) return;
-    if(red.state)ledcontroller_pwm_handler(red);
-    if(green.state)ledcontroller_pwm_handler(green);
-    if(blue.state)ledcontroller_pwm_handler(blue);
+    if(rgb[RED].state)ledcontroller_pwm_handler(RED);
+    if(rgb[GREEN].state)ledcontroller_pwm_handler(GREEN);
+    if(rgb[BLUE].state)ledcontroller_pwm_handler(BLUE);
     if(current_state==SWEEPING)ledcontroller_sweep_handler();
     if(current_state==BLINKING)ledcontroller_blink_handler();
 }
 
-void ledcontroller_pwm_handler(led_type led){
-    char true_intensity=(led.duty_cycle*led_intensity)/INTENSITY_SCALE;
-    if(led.cycle_iteration<(PWM_PERIOD/INTERRUPT_PERIOD-true_intensity))
-        led_off(led);
+void ledcontroller_pwm_handler(char led_index){
+    char true_intensity=(rgb[led_index].duty_cycle*led_intensity)/INTENSITY_SCALE;
+    if(rgb[led_index].cycle_iteration<(PWM_PERIOD/INTERRUPT_PERIOD-true_intensity))
+        led_off(led_index);
     else 
-        led_on(led);
-    led.cycle_iteration++;
-    if(led.cycle_iteration==PWM_PERIOD/INTERRUPT_PERIOD) led.cycle_iteration=0;          
+        led_on(led_index);
+    rgb[led_index].cycle_iteration++;
+    if(rgb[led_index].cycle_iteration==PWM_PERIOD/INTERRUPT_PERIOD) rgb[led_index].cycle_iteration=0;          
 }
 
 void ledcontroller_set_intensity(char intensity){
@@ -211,19 +210,19 @@ void ledcontroller_blink_handler(void){
     blink_counter=0;
     if(blink_state){
         blink_state=0;
-        red.previous_state=red.state;
-        green.previous_state=green.state;
-        blue.previous_state=blue.state;
+        rgb[RED].previous_state=rgb[RED].state;
+        rgb[GREEN].previous_state=rgb[GREEN].state;
+        rgb[BLUE].previous_state=rgb[BLUE].state;
         //desactiva todos, hasta los ya desactivados porque me daba paja poner ifs
-        led_desactivate(red);
-        led_desactivate(green);
-        led_desactivate(blue);
+        led_desactivate(RED);
+        led_desactivate(GREEN);
+        led_desactivate(BLUE);
     }else{
         blink_state=1;
         //activar solo los que estaban prendido previamente
-        if(red.previous_state)led_activate(red);
-        if(green.previous_state)led_activate(green);
-        if(blue.previous_state)led_desactivate(blue);
+        if(rgb[RED].previous_state)led_activate(RED);
+        if(rgb[GREEN].previous_state)led_activate(GREEN);
+        if(rgb[BLUE].previous_state)led_desactivate(BLUE);
     } 
 }
 void fBLINK_TOGGLE(void){
