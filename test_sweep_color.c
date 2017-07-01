f71c5d79a09d59898b705e1bbcb673574ca9d0c4
Agustin Sanchez <trorik23@gmail.com>
2017-07-01 19:48:50 -0300

    para testear el barrido

---
+++ test_sweep_color.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define MAX_DUTY_CYCLE 10
+#define COLORS (MAX_DUTY_CYCLE*3)
+
+int main(void){
+	const int dimy=MAX_DUTY_CYCLE;
+	const int dimx=COLORS;
+
+	int y, x;
+	FILE *fp = fopen("test.ppm", "wb");
+	(void) fprintf(fp, "P6\n%d %d\n255\n", dimx, dimy);
+	
+	/*
+	0	< rgb_duty_cycle	<=255
+	0	< color	<=MAX_DUTY_CYCLE
+	*/
+	for (y = 0; y < dimy; ++y){
+		unsigned char rgb_duty_cycle[3]={MAX_DUTY_CYCLE,0,0};
+		for (x = 0; x < dimx; ++x){
+			rgb_duty_cycle[x/MAX_DUTY_CYCLE]--;
+        	rgb_duty_cycle[((x/MAX_DUTY_CYCLE)+1)%3]++;
+			
+			static unsigned char color[3];
+			color[0] = (unsigned char)((255*rgb_duty_cycle[0])/MAX_DUTY_CYCLE);
+			color[1] = (unsigned char)((255*rgb_duty_cycle[1])/MAX_DUTY_CYCLE);
+			color[2] = (unsigned char)((255*rgb_duty_cycle[2])/MAX_DUTY_CYCLE);
+			printf("(%d/%d)-->(%d,%d,%d)\n",x,y,color[0],color[1],color[2]);
+			(void) fwrite(color, 1, 3, fp);
+		}
+	}
+	(void) fclose(fp);
+	return EXIT_SUCCESS;
+}
