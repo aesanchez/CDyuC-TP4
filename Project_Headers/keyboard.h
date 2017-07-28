4981877238667e15982b8f20b68c8f10216eaa91
Agustin Sanchez <trorik23@gmail.com>
2017-07-28 19:54:28 -0300

    keyboard comentado

--- Project_Headers/keyboard.h
+++ Project_Headers/keyboard.h
@@ -1,9 +1,18 @@
 #ifndef KEYBOARD_H_
 #define KEYBOARD_H_
 
-#define KEYBOARD_CHECK_PERIOD 30 //ms
+// KEYBOARD_CHECK_PERIOD indica el periodo con la que se va a llamar
+// a la rutina keyboard_check_key
+#define KEYBOARD_CHECK_PERIOD 30
 
+// keyboard_check_key se encarga de implementar el algoritmo de 
+// identificacion de la tecla presionada. Si se detecta el presionado
+// y liberado de una tecla se pushea el valor de la tecla al buffer
+// keyevent.
 void keyboard_check_key(void);
+
+// keyboard_init inicializa las configuraciones del puerto B
+// en el que esta conectado el teclado matricial
 void keyboard_init(void);
 
 #endif
--- Sources/keyboard.c
+++ Sources/keyboard.c
@@ -2,27 +2,39 @@
 #include "keyevent.h"
 #include "keyboard.h"
 
+// NOT_PRESSED es un valor de control utilizado para las funciones internas de keyboard
+// que indica que la columna o fila no esta presionada
 #define NOT_PRESSED 42 //meaning of life
+
+// EMPTY_KEY es lo correspondiente a un valor nulo
 const char EMPTY_KEY = ' ';
+
+// variables internas utilizadas para la identificacion de la tecla presionada
 char last_pressed_key;
 char pressed_row;
 char pressed_column;
 
+// mapeo de teclas correspondiente al teclado matricial
 const char keymap[4][4] = { { '1', '2', '3', 'A' },
                          { '4', '5', '6', 'B' },
                          { '7',	'8', '9', 'C' },
                          { '*', '0', '#', 'D' } };
 
-
-void check_column(void);
+void check_column(void); 
 void check_row(void);
 
+// keyboard_init inicializa las configuraciones del puerto B
+// en el que esta conectado el teclado matricial
 void keyboard_init(void){
 	PTBDD = 0x0F;
 	PTBPE = 0xF0;
 	last_pressed_key=EMPTY_KEY;
 }
 
+// keyboard_check_key se encarga de implementar el algoritmo de 
+// identificacion de la tecla presionada. Si se detecta el presionado
+// y liberado de una tecla se pushea el valor de la tecla al buffer
+// keyevent.
 void keyboard_check_key(void) {
 	check_column();
 	if (pressed_column == NOT_PRESSED) {
@@ -39,6 +51,8 @@ void keyboard_check_key(void) {
 	last_pressed_key = keymap[pressed_row][pressed_column];
 }
 
+// check_row de forma analoga, devuelve en pressed_row la fila correspondiente a la 
+// tecla presionada. Realiza la deteccion, realizando un "barrido" de filas.
 void check_row(void) {
 	char current_row = 0;
 	while (current_row < 4) {
@@ -68,6 +82,9 @@ void check_row(void) {
 	pressed_row = NOT_PRESSED;
 }
 
+// check_column realiza el primer chequeo para la identificacion de la tecla presionada.
+// Devuelve en pressed_column la columna correspondiente a la tecla presionada, si es
+// que lo hubiese
 void check_column(void) {
 	if (PTBD_PTBD7 == 0) {
 		pressed_column = 3;
